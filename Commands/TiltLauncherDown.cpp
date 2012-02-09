9e630b3f9a0835e6f2f9b1af58bc8bd3e4817301
unknown <azrathud@bob.(none)>
2012-02-09 16:03:45 -0700

    set default command typo. Now builds. Changed enum usage for test drive code

---
+++ Commands/TiltLauncherDown.cpp
@@ -0,0 +1,31 @@
+#include "TiltLauncherDown.h"
+
+TiltLauncherDown::TiltLauncherDown() {
+	// Use requires() here to declare subsystem dependencies
+	// eg. requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void TiltLauncherDown::Initialize() {
+	
+}
+
+// Called repeatedly when this Command is scheduled to run
+void TiltLauncherDown::Execute() {
+	
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool TiltLauncherDown::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void TiltLauncherDown::End() {
+	
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void TiltLauncherDown::Interrupted() {
+}
---
+++ OI.cpp
@@ -0,0 +1,18 @@
+#include "OI.h"
+
+OI::OI() {
+	// Process operator interface input here.
+	driverStick = new Joystick(DRIVER_JOYSTICK_PORT);
+	launcherStick = new Joystick(LAUNCHER_JOYSTICK_PORT);
+	
+	}
+
+Joystick* OI::getDriverJoystick()
+{
+	return driverStick;
+}
+
+Joystick* OI::getLauncherJoystick()
+{
+	return launcherStick;
+}
