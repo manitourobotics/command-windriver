2cb003cf40dcea299bff593ee251a5556dae1089
Josh <jtwomey423@gmail.com>
2012-02-12 12:12:55 -0700

    accidently didn't add the camera code; Now it is added.

---
+++ Subsystems/Camera.cpp
@@ -0,0 +1,18 @@
+#include "Camera.h"
+#include "../Robotmap.h"
+
+Camera::Camera() : Subsystem("Camera") {
+
+	camera = &(AxisCamera::GetInstance());
+	camera->WriteResolution(AxisCamera::kResolution_320x240);
+	camera->WriteBrightness(0);
+}
+    
+void Camera::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	//SetDefaultCommand(new MySpecialCommand());
+}
+
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ Subsystems/Camera.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+/**
+ *
+ *
+ * @author Bang
+ */
+class Camera: public Subsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	
+	AxisCamera* camera;
+	
+public:
+	Camera();
+	void InitDefaultCommand();
+};
+
+#endif
